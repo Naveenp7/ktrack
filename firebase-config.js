e38fd32e7e4d3ce7d86f36a5d8146330b31df219
Naveen <naveensanthosh830@gmail.com>
2025-07-27 22:58:06 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,8 +6,8 @@ const firebaseConfig = {
 
 // Telegram Bot Configuration (for demonstration purposes - consider server-side for production)
 window.telegramConfig = {
-  botToken: '8021979712:AAGq1_DUk10wwkDWHzyL9YV8KWSgcJAmKJU',
-  chatId: '1921751672' // Can be a group chat ID (e.g., -123456789) or user ID
+  botToken: ':',
+  chatId: '' // Can be a group chat ID (e.g., -123456789) or user ID
 };
 
 // Firebase will be initialized in app.js
